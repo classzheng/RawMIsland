2596eb54a2ddf6e1b42f519a83f6275c4552f331
classzheng <134850675+classzheng@users.noreply.github.com>
2024-07-21 06:34:45 +0800

    `3D-Map Foundation Update.🪽` @classzheng.

--- RawMIsland.h
+++ RawMIsland.h
@@ -5,127 +5,53 @@
  * RawMeat Island🥩             *
  ********************************/
 #include "FoundMeat.h"
-typedef enum {
-  BlaLine,HgIsland,Blood,Sky,Island,
-} StagMode;
-RgbStore* greateWorld(short wt,long long seed,StagMode stag) {
+GLmap GLgrtFillet(short wt,long long seed) {
 	RgbStore* mtr;
-	switch(stag) {
-	  case BlaLine:
-	  	mtr=(RgbStore*)malloc(sizeof(RgbStore)*wt*wt);
-	  	for(i=wt-10; i<wt; i++)
-	  	  for(j=0; j<wt; j++)
+	srand(seed),mtr=GFQcirc(wt,wt/4+rand()%7,wt*3/10,wt/40,rawm,rawrIm);
+	for(i=wt-10; i<wt; i++)
+	  for(j=0; j<wt; j++)
+	  	if(RgbEqual(mtr[i*wt+j],emp))
 	  		mtr[i*wt+j]=blood;
-		for(i=0; i<wt; i++)
-			for(j=0; j<wt; j++) if(RgbEqual(mtr[i*wt+j],emp))mtr[i*wt+j]=sky;
-		return mtr;
-	  case HgIsland:
-		srand(seed),mtr=GFQcirc(wt,wt/4+rand()%7,wt*3/10,wt/40,rawm,rrm);
-		for(i=wt-10; i<wt; i++)
-		  for(j=0; j<wt; j++)
-		  	if(RgbEqual(mtr[i*wt+j],emp))
-		  		mtr[i*wt+j]=blood;
-		  	else {
-		  		mtr[i*wt+j].red+=20;
-		  		if(!(rand()%7)) mtr[(i-1)*wt+j].red++;
-		  	}
-		for(i=0; i < wt; i++) {
-			if(rand()%2) {
-				for(j=0; j < wt; j++) {
-					if(RgbEqual(mtr[j*wt+i],rawm))     { mtr[j*wt+i]=emp; break; }
-					else if(RgbEqual(mtr[j*wt+i],blood)) break;
-				}
-			}
-		}
-		for(i=0; i < wt; i++) {
-			if(rand()%2) {
-				for(j=wt-1; j >= 0; j--) {
-					if(RgbEqual(mtr[j*wt+i],qrm))     { mtr[j*wt+i]=blood; break; }
-					else if(RgbEqual(mtr[j*wt+i],rawm)||RgbEqual(mtr[j*wt+i],emp)||RgbEqual(mtr[j*wt+i],rawm)) break;
-				}
-			}
-		}
-		for(i=0; i < wt; i++) {
+	  	else {
+	  		mtr[i*wt+j].red+=20;
+	  		if(!(rand()%7)) mtr[(i-1)*wt+j].red++;
+	  	}
+	for(i=0; i < wt; i++) {
+		if(rand()%2) {
 			for(j=0; j < wt; j++) {
-				if(RgbEqual(mtr[j*wt+i],rawm)&&rand()%10) {
-					bool reach=false;
-					if(true)     mtr[j*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[j*wt+i],emp)) mtr[j*wt+i]=mtr[j*wt+i-1];
-					if(rand()%5&&RgbEqual(mtr[j*wt+i],rawm)) reach=false;
-					if(rand()%5&&reach) mtr[(j+1)*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[(j+1)*wt+i],emp)) mtr[(j+1)*wt+i]=mtr[(j+1)*wt+i-1];
-					if(rand()%5&&reach) mtr[(j+2)*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[(j+2)*wt+i],emp)) mtr[(j+2)*wt+i]=mtr[(j+2)*wt+i-1];
-					break;
-				}
+				if(RgbEqual(mtr[j*wt+i],Brawm))     { mtr[j*wt+i]=emp; break; }
 				else if(RgbEqual(mtr[j*wt+i],blood)) break;
 			}
 		}
-		for(i=0; i<wt; i++)
-			for(j=0; j<wt; j++) if(RgbEqual(mtr[i*wt+j],emp))mtr[i*wt+j]=sky;
-		for(i=0; i<wt; i++)
-			for(j=0; j<wt; j++)
-				if((!RgbEqual(mtr[i*wt+j],sky))&&RgbEqual(mtr[(i+1)*wt+j],sky)) mtr[(i+1)*wt+j]=rawm;
-		return mtr;
-	  case Blood:
-	  	mtr=(RgbStore*)malloc(sizeof(RgbStore)*wt*wt);
-  	  	for(i=0; i<wt; i++)
-  	  	  for(j=0; j<wt; j++)
-  	  		mtr[i*wt+j]=blood;
-  	  	return mtr;
-	  case Sky:
-	  	mtr=(RgbStore*)malloc(sizeof(RgbStore)*wt*wt);
-  	  	for(i=0; i<wt; i++)
-  	  	  for(j=0; j<wt; j++)
-  	  		mtr[i*wt+j]=sky;
-  	  	return mtr;
-	  case Island:
-		srand(seed),mtr=GFQcirc(wt,(wt/4+rand()%7)/2,wt*3/20,wt/40,rawm,rrm);
-		for(i=wt-10; i<wt; i++)
-		  for(j=0; j<wt; j++)
-		  	if(RgbEqual(mtr[i*wt+j],emp))
-		  		mtr[i*wt+j]=blood;
-		  	else {
-		  		mtr[i*wt+j].red+=20;
-		  		if(!(rand()%7)) mtr[(i-1)*wt+j].red++;
-		  	}
-		for(i=0; i < wt; i++) {
-			if(rand()%2) {
-				for(j=0; j < wt; j++) {
-					if(RgbEqual(mtr[j*wt+i],rawm))     { mtr[j*wt+i]=emp; break; }
-					else if(RgbEqual(mtr[j*wt+i],blood)) break;
-				}
+	}
+	for(i=0; i < wt; i++) {
+		if(rand()%2) {
+			for(j=wt-1; j >= 0; j--) {
+				if(RgbEqual(mtr[j*wt+i],Bsfrawm))     { mtr[j*wt+i]=blood; break; }
+				else if(RgbEqual(mtr[j*wt+i],Brawm)||RgbEqual(mtr[j*wt+i],emp)||RgbEqual(mtr[j*wt+i],Brawm)) break;
 			}
 		}
-		for(i=0; i < wt; i++) {
-			if(rand()%2) {
-				for(j=wt-1; j >= 0; j--) {
-					if(RgbEqual(mtr[j*wt+i],qrm))     { mtr[j*wt+i]=blood; break; }
-					else if(RgbEqual(mtr[j*wt+i],rawm)||RgbEqual(mtr[j*wt+i],emp)||RgbEqual(mtr[j*wt+i],rawm)) break;
-				}
+	}
+	for(i=0; i < wt; i++) {
+		for(j=0; j < wt; j++) {
+			if(RgbEqual(mtr[j*wt+i],Brawm)&&rand()%10) {
+				bool reach=false;
+				if(true)     mtr[j*wt+i]=Bcookdm,reach=true;
+				if(!(rand()%3)&&!RgbEqual(mtr[j*wt+i],emp)) mtr[j*wt+i]=mtr[j*wt+i-1];
+				if(rand()%5&&RgbEqual(mtr[j*wt+i],Brawm)) reach=false;
+				if(rand()%5&&reach) mtr[(j+1)*wt+i]=Bcookdm,reach=true;
+				if(!(rand()%3)&&!RgbEqual(mtr[(j+1)*wt+i],emp)) mtr[(j+1)*wt+i]=mtr[(j+1)*wt+i-1];
+				if(rand()%5&&reach) mtr[(j+2)*wt+i]=Bcookdm,reach=true;
+				if(!(rand()%3)&&!RgbEqual(mtr[(j+2)*wt+i],emp)) mtr[(j+2)*wt+i]=mtr[(j+2)*wt+i-1];
+				break;
 			}
+			else if(RgbEqual(mtr[j*wt+i],blood)) break;
 		}
-		for(i=0; i < wt; i++) {
-			for(j=0; j < wt; j++) {
-				if(RgbEqual(mtr[j*wt+i],rawm)&&rand()%10) {
-					bool reach=false;
-					if(true)     mtr[j*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[j*wt+i],emp)) mtr[j*wt+i]=mtr[j*wt+i-1];
-					if(rand()%5&&RgbEqual(mtr[j*wt+i],rawm)) reach=false;
-					if(rand()%5&&reach) mtr[(j+1)*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[(j+1)*wt+i],emp)) mtr[(j+1)*wt+i]=mtr[(j+1)*wt+i-1];
-					if(rand()%5&&reach) mtr[(j+2)*wt+i]=cdm,reach=true;
-					if(!(rand()%3)&&!RgbEqual(mtr[(j+2)*wt+i],emp)) mtr[(j+2)*wt+i]=mtr[(j+2)*wt+i-1];
-					break;
-				}
-				else if(RgbEqual(mtr[j*wt+i],blood)) break;
-			}
-		}
-		for(i=0; i<wt; i++)
-			for(j=0; j<wt; j++) if(RgbEqual(mtr[i*wt+j],emp))mtr[i*wt+j]=sky;
-		for(i=0; i<wt; i++)
-			for(j=0; j<wt; j++)
-				if((!RgbEqual(mtr[i*wt+j],sky))&&RgbEqual(mtr[(i+1)*wt+j],sky)) mtr[(i+1)*wt+j]=rawm;
-		return mtr;
-	};
+	}
+	for(i=0; i<wt; i++)
+		for(j=0; j<wt; j++) if(RgbEqual(mtr[i*wt+j],emp))mtr[i*wt+j]=Bsky;
+	for(i=0; i<wt; i++)
+		for(j=0; j<wt; j++)
+			if((!RgbEqual(mtr[i*wt+j],Bsky))&&RgbEqual(mtr[(i+1)*wt+j],Bsky)) mtr[(i+1)*wt+j]=Brawm;
+	return mtr;
 }
